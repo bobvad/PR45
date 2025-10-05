744ceebba796d2b7c8ee8a4603326f832bb2ee01
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-10-06 01:30:42 +0500

    Разработанные модели данных.Дегтянников

---
+++ APIS_Degtiannikov/Models/Task.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace APIS_Degtiannikov.Models
+{
+    /// <summary>
+    /// Модель задачи
+    /// </summary>
+    public class Task
+    {
+        /// <summary>
+        /// ID задачи
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название задачи
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Приоритет задачи
+        /// </summary>
+        public string Priority { get; set; }
+
+        /// <summary>
+        /// Дата выполнения задачи
+        /// </summary>
+        public DateTime DateExecute { get; set; }
+
+        /// <summary>
+        /// Комментарий к задаче
+        /// </summary>
+        public string Comment { get; set; }
+
+        /// <summary>
+        /// Статус выполнения задачи
+        /// </summary>
+        public bool Done { get; set; }
+    }
+}

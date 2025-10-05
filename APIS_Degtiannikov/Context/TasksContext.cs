6c63bf1dba94754192a6313e60bfcfe77bcdd75b
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-10-06 01:37:00 +0500

    Разработанный контекст для загрузки данных.Дегтянников.

---
+++ APIS_Degtiannikov/Context/TasksContext.cs
@@ -0,0 +1,20 @@
+﻿using APIS_Degtiannikov.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace APIS_Degtiannikov.Context
+{
+    public class TasksContext: DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }  
+        public TasksContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server = 127.0.0.1;uid=root;database=TaskManager",new MySqlServerVersion(new Version(8,0,11)));
+        }
+    }
+}

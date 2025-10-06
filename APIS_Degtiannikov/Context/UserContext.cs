983e4009c6087f2058efb899099b05b654863eaf
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-10-06 12:40:00 +0500

    Создание модели и контекста пользователя.Дегтянников

---
+++ APIS_Degtiannikov/Context/UserContext.cs
@@ -0,0 +1,20 @@
+﻿using APIS_Degtiannikov.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace APIS_Degtiannikov.Context
+{
+    public class UserContext: DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UserContext()
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server = 127.0.0.1;uid=root;database=TaskManager", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
